bbaec536df3b4d8e4fa7813d400a342ee48427bc
mjiw <113112060+mjiw@users.noreply.github.com>
2022-10-18 16:53:28 +0900

    test 4

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void f(void);
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-//프로토 타입 선언 해줘야함
-int inc(int counter); //프로토타입 
- 
+int i;
 int main(int argc, char *argv[]) {
-	int i=10;
-	printf("함수 호출 전 i=%d\n",i);
-	inc(i);
-	printf("함수 호출 후 i=%d\n",i);
+	for(i=0;i<5;i++){
+		f();
+	}
 	return 0;
 }
-
-int inc(int counter){
-	counter++;
-	return counter;
+void f(void){
+	for(i=0;i<10;i++){
+		printf("#");
+	}
 }
