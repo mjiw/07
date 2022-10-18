7f31cb68acea9da8939bc9cf2e3b55ff3c10e513
mjiw <113112060+mjiw@users.noreply.github.com>
2022-10-18 16:39:48 +0900

    test 3

--- main.c
+++ main.c
@@ -3,14 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+//프로토 타입 선언 해줘야함
+int inc(int counter); //프로토타입 
+ 
 int main(int argc, char *argv[]) {
-	int i;
-
-	for(i=0;i<5;i++){
-		int temp=1;
-		printf("temp = %d\n",temp);
-		temp++;
-	}
-	
+	int i=10;
+	printf("함수 호출 전 i=%d\n",i);
+	inc(i);
+	printf("함수 호출 후 i=%d\n",i);
 	return 0;
+}
+
+int inc(int counter){
+	counter++;
+	return counter;
 }
