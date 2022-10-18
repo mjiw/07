70b448a36e66800fd68c6f4ac91f5787e37d05c7
mjiw <113112060+mjiw@users.noreply.github.com>
2022-10-18 17:12:11 +0900

    test 6-1

--- main.c
+++ main.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-void f(void);
+int all_files;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int i;
+extern void sub();
+
 int main(int argc, char *argv[]) {
-	for(i=0;i<5;i++){
-		f();
-	}
+	sub();
+	printf("%d\n",all_files);
 	return 0;
 }
-void f(void){
-	for(i=0;i<10;i++){
-		printf("#");
-	}
-}
+
+
