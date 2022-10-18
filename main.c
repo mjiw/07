8d96c22c13519a87bbc2b8c9e1494def4752b549
mjiw <113112060+mjiw@users.noreply.github.com>
2022-10-18 18:58:58 +0900

    test 5

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-int all_files;
+void sub(void);
+
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-extern void sub();
 
-int main(int argc, char *argv[]) {
-	sub();
-	printf("%d\n",all_files);
+int main(void) {
+	int i;
+	for(i=o;i<3;i++){ 
+	sub(); 
+	}
+
 	return 0;
 }
 
-
+void sub(void){
+	int auto_count=0;
+	static int static_count=0;
+	auto_count++;
+	static_count++;
+	printf("auto_count=%d\n",auto_count);
+	printf("static_count=%d\n",static_count);
+}
